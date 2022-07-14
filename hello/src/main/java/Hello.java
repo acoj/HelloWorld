3ce5e84a6b04048526f1e38c5029326686655a18
Christian19960330 <Christian19960330@protonmail.com>
2022-07-14 15:57:20 +0800

    Changes

---
+++ hello/src/main/java/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+        System.out.println("Hello git1");
+        System.out.println("create branch");
+        System.out.println("dev update");
+        System.out.println("master update");
+        System.out.println("dev update");
+        System.out.println("online update");
+        System.out.println("other update");
+        System.out.println("test ssh");
+    }
+
+
+}
